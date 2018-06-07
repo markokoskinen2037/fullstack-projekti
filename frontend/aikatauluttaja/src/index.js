cbf3bffad453adc318c93156660ef00104bde8c3
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-07 14:36:11 +0300

    Sijoitettu <App/> React.Fragmentin sisälle

--- frontend/aikatauluttaja/src/index.js
+++ frontend/aikatauluttaja/src/index.js
@@ -2,7 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+//import CssBaseline from '@material-ui/core/CssBaseline';
+
+
 ReactDOM.render(
-  <App />,
+  <React.Fragment>
+  
+  <App />
+  </React.Fragment>,
   document.getElementById('root')
 )
