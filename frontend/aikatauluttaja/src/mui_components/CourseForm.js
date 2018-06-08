98d21c7a4ed99a44f4a3c1ee9706142f3f9273aa
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-08 12:51:53 +0300

    parannettu ulkoasua

--- frontend/aikatauluttaja/src/mui_components/CourseForm.js
+++ frontend/aikatauluttaja/src/mui_components/CourseForm.js
@@ -86,7 +86,7 @@ class CourseForm extends React.Component {
 
             <Fragment>
                 <Grid item xs={12}>
-                    <Paper style={{padding: 0, marginTop: 10, marginLeft: 10, marginRight: 10}}>
+                    <Paper style={{padding: 0, marginTop: 10, marginLeft: 10, marginRight: 10, paddingTop: 10}}>
                 
                 
 
