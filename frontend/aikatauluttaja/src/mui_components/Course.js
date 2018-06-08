11734c3211c52ae7d54c237d3f35aa0b76adc92a
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-08 18:36:56 +0300

    parannettu ulkoasua. korvattu Muokkaa teksti edit-iconilla

--- frontend/aikatauluttaja/src/mui_components/Course.js
+++ frontend/aikatauluttaja/src/mui_components/Course.js
@@ -68,13 +68,6 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
   }
 
   if(user.activeCourses.find(aktiivinen => aktiivinen._id === course._id)){
-
-
-
-
-
-
-
     return (
         <Fragment>
             <Grid item xs={12}>
@@ -84,8 +77,8 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
                             <ListItemText primary={course.title} />
                             <Typography style={{marginRight: 50}} variant="button">{course.credits} op</Typography>
                             <Typography variant="button">{course.length} periodia</Typography>
-                            <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}>Muokkaa</Link></Button>
-                            <Button onClick={() => deleteCourse(course._id)}>Poista</Button>
+                            <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}><i className="material-icons">edit</i></Link></Button>
+                            <Button onClick={() => deleteCourse(course._id)}><i className="material-icons">delete</i></Button>
                             <Button onClick={() => toggleActive(course._id)}><i className="material-icons">star</i></Button>
                             </ListItem>
 
@@ -101,15 +94,15 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
   } else {
     return (
         <Fragment>
-            <Grid item xs={12}>
+            <Grid item xs={12} >
                     <Paper>
                         
                             <ListItem>
                             <ListItemText primary={course.title} />
-                            <Typography style={{marginRight: 50}} variant="button">{course.credits} op</Typography>
-                            <Typography variant="button">{course.length} periodia</Typography>
-                            <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}>Muokkaa</Link></Button>
-                            <Button onClick={() => deleteCourse(course._id)}>Poista</Button>
+                            <Button><Typography variant="button">{course.credits} op</Typography></Button>
+                            <Button><Typography variant="button">{course.length} periodia</Typography></Button>
+                            <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}><i className="material-icons">edit</i></Link></Button>
+                            <Button onClick={() => deleteCourse(course._id)}><i className="material-icons">delete</i></Button>
                             <Button onClick={() => toggleActive(course._id)}><i className="material-icons">star_border</i></Button>
                             </ListItem>
 
