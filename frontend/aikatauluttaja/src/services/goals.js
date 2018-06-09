3490bf004540fc8ebd67498a1da89651458a553d
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-09 13:00:31 +0300

    luotu goals.js

---
+++ frontend/aikatauluttaja/src/services/goals.js
@@ -0,0 +1,41 @@
+import axios from "axios"
+const baseUrl = "/api/goals"
+
+
+let token = null
+
+const setToken = (newToken) => {
+    token = `bearer ${newToken}`
+  }
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const get = (id) => {
+    const request = axios.get(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const create = async (newObject) => {
+    const config = {
+      headers: { 'Authorization': token }
+    }
+  
+    const response = await axios.post(baseUrl, newObject, config)
+    return response
+  }
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+const removeById = (id) => {
+    axios.delete(`${baseUrl}/${id}`)
+    
+}
+
+
+export default { getAll, create, update, get, removeById, setToken }
