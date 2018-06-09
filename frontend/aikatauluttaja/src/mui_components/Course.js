21837de76da900ec963b254d5d7ae8cd947be4bf
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-09 15:31:10 +0300

    turhat rivinvaihdot pois, alettu toteuttaa tavoitteiden asettamista

--- frontend/aikatauluttaja/src/mui_components/Course.js
+++ frontend/aikatauluttaja/src/mui_components/Course.js
@@ -57,36 +57,53 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
 
   }
 
+  const getGoalValue = () => {
+
+    console.log(user.goals)
+
+    let foundGoal
+
+    if(user.goals === undefined){
+      foundGoal === undefined
+    } else {
+      foundGoal = user.goals.find(goal => goal.course === course.id)
+    }
+
+
+
+    if(foundGoal === undefined){ //Kirjautunut käyttäjä ei ole asettanut tälle kurssille tavoitetta
+      return "Aseta tavoite"
+    } else {
+      return foundGoal.target
+    }
+
+  }
+
+
   if(user.activeCourses.find(aktiivinen => aktiivinen._id === course._id)){
     return (
         <Fragment>
             <Grid item xs={12}>
                     <Paper>
-                        
                             <ListItem>
-                            <ListItemText primary={course.title} />
+                                <ListItemText primary={course.title} />
 
-                            <Button onClick={() => deleteCourse(course._id)}>TODO</Button>
+                                <Typography  style={{marginRight: 50}} variant="button">{getGoalValue()}</Typography>
 
 
-                            <Typography style={{marginRight: 50}} variant="button">{course.credits} op</Typography>
+                                <Typography style={{marginRight: 50}} variant="button">{course.credits} op</Typography>
 
-                            <Typography variant="button">{course.length} periodia</Typography>
-                            <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}><i className="material-icons">edit</i></Link></Button>
-                            <Button onClick={() => deleteCourse(course._id)}><i className="material-icons">delete</i></Button>
-                            <Button onClick={() => toggleActive(course._id)}><i className="material-icons">star</i></Button>
+                                <Typography variant="button">{course.length} periodia</Typography>
+                                <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}><i className="material-icons">edit</i></Link></Button>
+                                <Button onClick={() => deleteCourse(course._id)}><i className="material-icons">delete</i></Button>
+                                <Button onClick={() => toggleActive(course._id)}><i className="material-icons">star</i></Button>
                             </ListItem>
-
                     </Paper>
             </Grid>
         </Fragment>
-
-
-
-
-
     )
   } else {
+
     return (
         <Fragment>
             <Grid item xs={12} >
@@ -104,21 +121,8 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
                     </Paper>
             </Grid>
         </Fragment>
-
-
-
-
     )
-
-
-
-
-
-
   }
-  
-
-
 }
 
 export default Course
