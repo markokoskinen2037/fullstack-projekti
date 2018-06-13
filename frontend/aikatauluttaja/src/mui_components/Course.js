7d51685a24e4675cd8403cf9170094a6564ac0c1
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-13 15:36:36 +0300

    jos kurssilla ja käyttäjällä ei ole yhteyttä luodaan uusi goal

--- frontend/aikatauluttaja/src/mui_components/Course.js
+++ frontend/aikatauluttaja/src/mui_components/Course.js
@@ -33,11 +33,12 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
 
 
     if(user.activeCourses.find(course => course._id === course_id)){ //Jos aktivoitava kurssi on listalla
+      console.log("deactivating...")
 
       var newList = user.activeCourses.filter(course => course._id !== course_id) //Poistetaan kurssi aktiivisten listasta
       user.activeCourses = newList
     } else { //Jos aktivoitava kurssi ei ole listalla
-      console.log("activating")
+      console.log("activating...")
 
 
       user.activeCourses = user.activeCourses.concat(course) //Lisätään aktivoitava kurssi listalle
@@ -45,8 +46,7 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
 
     let uusiAktiivistenKurssienLista = {activeCourses: user.activeCourses}
 
-    //console.log(user.activeCourses)
-    //console.log(test)
+
     
 
     userService
@@ -58,25 +58,56 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
 
   }
 
-  const getGoalValue = () => {
+  const getGoalFieldContent = () => {
 
     console.log("Current goals in state: " + user.goals)
 
-    let foundGoal
+    let foundGoal = undefined
 
     if(user.goals === undefined){
       foundGoal === undefined
-    } else {
-      foundGoal = user.goals.find(goal => goal.course === course.id)
+    } else { //Jos userin goal listalla on jotain tarkistetaan onko kyseessä juuri tätä kurssia koskeva goal
+      foundGoal = user.goals.find(goal => goal.course === course._id)
+      console.log("goal already found for this course: " + foundGoal)
     }
 
 
 
     if(foundGoal === undefined){ //Kirjautunut käyttäjä ei ole asettanut tälle kurssille tavoitetta
-      return "Aseta tavoite"
+
+
+      //Luodaan uusi tavoite oletusarvolla 1 
+      //Tallennetaan tietokantaan
+
+
+      const goalToBeCreated = {
+        courseid: course._id,
+        userid: user._id,
+        target: 1
+      }
+
+      console.log("new goal object created!")
+
+
+      goalService
+      .create(goalToBeCreated)
+      .then(response => {
+        console.log("new goal saved to database!")
+        //console.log(response)
+
+        //user.state.goals.add <=======> response.data._id
+        return(response)
+      })
+
+
+      
     } else {
       return foundGoal.target
     }
+
+  }
+
+  const scrollGoalValue = () =>{
 
   }
 
@@ -89,7 +120,7 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
                             <ListItem>
                                 <ListItemText primary={course.title} />
 
-                                <Typography  style={{marginRight: 50}} variant="button">{getGoalValue()}</Typography>
+                                <Typography  style={{marginRight: 50}} variant="button">{getGoalFieldContent()}</Typography>
 
 
                                 <Typography style={{marginRight: 50}} variant="button">{course.credits} op</Typography>
@@ -112,6 +143,10 @@ const Course = ({ reloadCoursesFromBackend, updateUserState, findCourse, removeC
                         
                             <ListItem>
                             <ListItemText primary={course.title} />
+
+                            <Typography  style={{marginRight: 50}} variant="button">{getGoalFieldContent()}</Typography>
+
+
                             <Button><Typography variant="button">{course.credits} op</Typography></Button>
                             <Button><Typography variant="button">{course.length} periodia</Typography></Button>
                             <Button><Link style={{color: 'inherit'}} to={`/courses/${course._id}`}><i className="material-icons">edit</i></Link></Button>
