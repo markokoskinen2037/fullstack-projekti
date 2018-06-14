39aeed499d5562f7ec796f1364ca2363ccdbabb2
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-14 16:00:03 +0300

    ei turhaan renderöidä ilmoitusta jos käyttäjä ei ole kirjautunut

--- frontend/aikatauluttaja/src/App.js
+++ frontend/aikatauluttaja/src/App.js
@@ -255,7 +255,7 @@ class App extends React.Component {
                     )
                   } else {
                     return (
-                      <p>Please login to view and modify courses.</p>
+                      null
                     )
                   }
                 }
