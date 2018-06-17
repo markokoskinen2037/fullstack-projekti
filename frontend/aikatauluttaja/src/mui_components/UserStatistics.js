ff32246660831f2871d692cceef9acc269427f96
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-17 15:51:29 +0300

    lisätty enemmän renderöitävää nippelitietoa

--- frontend/aikatauluttaja/src/mui_components/UserStatistics.js
+++ frontend/aikatauluttaja/src/mui_components/UserStatistics.js
@@ -8,10 +8,10 @@ class UserStatistics extends Component {
     constructor(props) {
       super(props)
 
-      const user = JSON.parse(window.localStorage.getItem("user"))
+
     
       this.state = {
-         user : user
+         user : this.props.getLoggedInUser()
       }
     }
 
@@ -23,21 +23,27 @@ class UserStatistics extends Component {
   render() {
 
 
+
+
     if(this.state.user === null){
         return null
     } else {
+
+        console.log(JSON.stringify(this.state.user))
 
 
         return (
             <Fragment>
               <Grid item md={6} xs={12}>
                   <Paper style={{padding: 10, marginTop: 10, marginLeft: 10, marginRight: 10}} elevation={4}>
-                      <Typography variant="headline">User statistics:</Typography>
+                      <Typography variant="headline">Käyttäjätietosi:</Typography>
       
                       <div style={{marginLeft: 10}}>
-                          <Typography style={{marginTop: 20}} paragraph={true} variant="body1">Username: {this.state.user.username}</Typography>
+                          <Typography style={{marginTop: 20}} paragraph={true} variant="body1">Käyttäjätunnus: {this.state.user.username}</Typography>
                           <Typography paragraph={true} variant="body1">Id: {this.state.user._id}</Typography>
-                          <Typography paragraph={true} variant="body1">Email: {this.state.user.email}</Typography>
+                          <Typography paragraph={true} variant="body1">Sähköposti: {this.state.user.email}</Typography>
+                          <Typography paragraph={true} variant="body1">Aktiivisia kursseja: {this.state.user.activeCourses.length}</Typography>
+                          <Typography paragraph={true} variant="body1">Asetettuja tavoitteita: {this.state.user.goals.length}</Typography>
                       </div>
       
                   </Paper>
