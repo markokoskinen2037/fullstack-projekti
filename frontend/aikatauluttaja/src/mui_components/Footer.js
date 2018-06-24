7474fef120203c6213e9ca4d5ced955c8457971d
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-06-24 14:48:47 +0300

    Tekijän nimi ja email

---
+++ frontend/aikatauluttaja/src/mui_components/Footer.js
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react'
+import {Paper, Typography} from "@material-ui/core"
+
+
+export default class Footer extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+  render() {
+    return (
+        <Fragment>
+            <Paper style={{marginTop: 100, width: "100%", padding:20, textAlign: "center"}}>
+            <Typography variant="body1">
+                Author: Marko Koskinen | Contact information: <a href="mailto:marko.j.koskinen@helsinki.fi">marko.j.koskinen@helsinki.fi</a>
+            </Typography>
+            </Paper>
+        </Fragment>
+    )
+  }
+}
