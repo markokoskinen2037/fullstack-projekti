243d5d95f0ece2481dfc8418a32d76e4a9946c15
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-07-17 17:06:42 +0300

    yhtenäistetty värimailmaa

--- frontend/aikatauluttaja/src/mui_components/Footer.js
+++ frontend/aikatauluttaja/src/mui_components/Footer.js
@@ -14,9 +14,9 @@ export default class Footer extends Component {
     render() {
         return (
             <Fragment>
-                <Paper style={{ marginTop: 100, width: "100%", padding: 2, textAlign: "center", bottom: 0, left: 0, position: "fixed" }}>
-                    <Typography variant="body1">
-                        Author: Marko Koskinen | Contact information: <a href="mailto:marko.j.koskinen@helsinki.fi">marko.j.koskinen@helsinki.fi</a> | <a href="https://github.com/markokoskinen2037/fullstack-projekti">GITHUB</a>
+                <Paper style={{ backgroundColor: "#4056b6", marginTop: 100, width: "100%", padding: 2, textAlign: "center", bottom: 0, left: 0, position: "fixed" }}>
+                    <Typography variant="body1" style={{ color: "white" }}>
+                        Author: Marko Koskinen | Contact information: <a style={{ color: "white" }} href="mailto:marko.j.koskinen@helsinki.fi">marko.j.koskinen@helsinki.fi</a> | <a style={{ color: "white" }} href="https://github.com/markokoskinen2037/fullstack-projekti">GITHUB</a>
                     </Typography>
                 </Paper>
             </Fragment>
