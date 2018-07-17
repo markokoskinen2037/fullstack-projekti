9edf8169a29c5b38f350f501128d71fd0b1f46c9
markokoskinen2037 <markokoskinen2037@gmail.com>
2018-07-17 15:44:01 +0300

    Lisätty linkki githubiin

--- frontend/aikatauluttaja/src/mui_components/Footer.js
+++ frontend/aikatauluttaja/src/mui_components/Footer.js
@@ -16,7 +16,7 @@ export default class Footer extends Component {
             <Fragment>
                 <Paper style={{ marginTop: 100, width: "100%", padding: 2, textAlign: "center", bottom: 0, left: 0, position: "fixed" }}>
                     <Typography variant="body1">
-                        Author: Marko Koskinen | Contact information: <a href="mailto:marko.j.koskinen@helsinki.fi">marko.j.koskinen@helsinki.fi</a>
+                        Author: Marko Koskinen | Contact information: <a href="mailto:marko.j.koskinen@helsinki.fi">marko.j.koskinen@helsinki.fi</a> | <a href="https://github.com/markokoskinen2037/fullstack-projekti">GITHUB</a>
                     </Typography>
                 </Paper>
             </Fragment>
