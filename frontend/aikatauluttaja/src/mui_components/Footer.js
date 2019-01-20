89fc0b661bd8c0f470bf26abc43299851de578c1
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-20 22:19:43 +0200

    prettier

--- frontend/aikatauluttaja/src/mui_components/Footer.js
+++ frontend/aikatauluttaja/src/mui_components/Footer.js
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from "react";
-import { Paper, Typography } from "@material-ui/core";
+import React, { Component, Fragment } from 'react'
+import { Paper, Typography } from '@material-ui/core'
 
 export default class Footer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {};
+    this.state = {}
   }
 
   render() {
@@ -13,27 +13,27 @@ export default class Footer extends Component {
       <Fragment>
         <Paper
           style={{
-            backgroundColor: "#4056b6",
+            backgroundColor: '#4056b6',
             marginTop: 100,
-            width: "100%",
+            width: '100%',
             padding: 2,
-            textAlign: "center",
+            textAlign: 'center',
             bottom: 0,
             left: 0,
-            position: "fixed"
+            position: 'fixed',
           }}
         >
-          <Typography variant="body1" style={{ color: "white" }}>
-            Author: Marko Koskinen | Contact information:{" "}
+          <Typography variant="body1" style={{ color: 'white' }}>
+            Author: Marko Koskinen | Contact information:{' '}
             <a
-              style={{ color: "white" }}
+              style={{ color: 'white' }}
               href="mailto:marko.j.koskinen@helsinki.fi"
             >
               marko.j.koskinen@helsinki.fi
-            </a>{" "}
-            |{" "}
+            </a>{' '}
+            |{' '}
             <a
-              style={{ color: "white" }}
+              style={{ color: 'white' }}
               href="https://github.com/markokoskinen2037/fullstack-projekti"
             >
               GITHUB
@@ -41,6 +41,6 @@ export default class Footer extends Component {
           </Typography>
         </Paper>
       </Fragment>
-    );
+    )
   }
 }
--- frontend/aikatauluttaja/src/mui_components/HomePage.js
+++ frontend/aikatauluttaja/src/mui_components/HomePage.js
@@ -1,53 +1,53 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react'
 
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
-import userService from "../services/users";
+import userService from '../services/users'
 
 class HomePage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       totalCredits: 0,
-      user: undefined
-    };
+      user: undefined,
+    }
   }
 
   componentDidMount() {
     if (this.props.user === null) {
-      if (window.localStorage.getItem("user") !== null) {
+      if (window.localStorage.getItem('user') !== null) {
         const userFromLocalStorage = JSON.parse(
-          window.localStorage.getItem("user")
-        );
+          window.localStorage.getItem('user')
+        )
 
         userService
           .get(userFromLocalStorage._id)
           .then(user => {
-            this.setState({ user });
-            console.table(this.state.user);
+            this.setState({ user })
+            console.table(this.state.user)
           })
           .then(res => {
             //Lasketaan paljonko on aktiivisten kurssien opintopisteiden summa ja tallennetaan tulos stateen
-            let sum = 0;
+            let sum = 0
             this.state.user.activeCourses.map(course => {
-              sum = sum + course.credits;
-              return sum;
-            });
-            this.setState({ totalCredits: sum });
-          });
+              sum = sum + course.credits
+              return sum
+            })
+            this.setState({ totalCredits: sum })
+          })
       }
     } else {
       //Saatiin user olio propsina joten käytetään sitä.
-      this.setState({ user: this.props.user });
+      this.setState({ user: this.props.user })
 
-      let sum = 0;
+      let sum = 0
       this.props.user.activeCourses.map(course => {
-        sum = sum + course.credits;
-        return sum;
-      });
-      this.setState({ totalCredits: sum });
+        sum = sum + course.credits
+        return sum
+      })
+      this.setState({ totalCredits: sum })
     }
   }
 
@@ -56,23 +56,23 @@ class HomePage extends React.Component {
       //Jos user on null, renderöidään kirjautumattoman käyttäjän etusivu:
       return (
         <Fragment>
-          <Grid item xs={12} md={8} style={{ margin: "auto" }}>
+          <Grid item xs={12} md={8} style={{ margin: 'auto' }}>
             <Paper
               style={{
-                margin: "auto",
+                margin: 'auto',
                 padding: 10,
                 marginTop: 10,
                 marginLeft: 10,
-                marginRight: 10
+                marginRight: 10,
               }}
               elevation={4}
             >
-              <Typography style={{ padding: "10px" }} variant="headline">
+              <Typography style={{ padding: '10px' }} variant="headline">
                 Tervetuloa suunnitelemaan opintojasi!
               </Typography>
 
               <Typography
-                style={{ marginLeft: "10px" }}
+                style={{ marginLeft: '10px' }}
                 paragraph={true}
                 variant="body1"
               >
@@ -89,47 +89,50 @@ class HomePage extends React.Component {
             </Paper>
           </Grid>
         </Fragment>
-      );
+      )
     } else if (this.state.user !== undefined) {
       //Jos käyttäjä ole olemassa, rendöidään sen tiedot ja suosituksia yms.
       return (
         <Fragment>
           <Typography
-            style={{ textAlign: "center", marginBottom: 20 }}
+            style={{ textAlign: 'center', marginBottom: 20 }}
             variant="display1"
           >
             Moi, {this.state.user.username}!
           </Typography>
 
-          <Grid item md={6} xs={12} style={{ margin: "auto" }}>
+          <Grid item md={6} xs={12} style={{ margin: 'auto' }}>
             <Paper style={{ padding: 30, marginTop: 20 }} elevation={1}>
               <Typography paragraph={true} variant="body1">
                 Sinulla on {this.props.user.activeCourses.length} aktiivista
                 kurssia.
                 <br />
-                Kyseiset kurssit suorittamalla ansaitset yhteensä{" "}
-                {this.state.totalCredits} opintopistettä. Olet määrittänyt{" "}
-                {this.state.user.goals.length} kurssitavoitetta{" "}
+                Kyseiset kurssit suorittamalla ansaitset yhteensä{' '}
+                {this.state.totalCredits} opintopistettä.
+                <br />
+                Olet määrittänyt {
+                  this.state.user.goals.length
+                } kurssitavoitetta{' '}
               </Typography>
             </Paper>
           </Grid>
 
-          <Grid item md={6} xs={12} style={{ margin: "auto" }}>
+          <Grid item md={6} xs={12} style={{ margin: 'auto' }}>
             <Paper style={{ padding: 30, marginTop: 20 }} elevation={1}>
               <Typography paragraph={true} variant="body1">
                 Vuosittainen tavoiteopintopistemäärä on 60 op. <br />
                 Pakollinen opintojen suoritusmäärä lukuvuotta kohden 1.8-31.7 on
                 20 op. <br />
                 {this.state.totalCredits < 60 &&
-                  "Vaikuttaa siltä, että et ole vielä suunnitellut 60 opintopisteen edestä opintoja tälle vuodelle."}
+                  'Vaikuttaa siltä, että et ole vielä suunnitellut 60 opintopisteen edestä opintoja tälle vuodelle.'}
                 <br />
                 {this.state.totalCredits < 20 &&
-                  " Vaikuttaa siltä, että opintosi eivät riitä 20 opintopisteen tavoitteeseen."}
+                  ' Vaikuttaa siltä, että opintosi eivät riitä 20 opintopisteen tavoitteeseen.'}
               </Typography>
             </Paper>
           </Grid>
 
-          <Grid item md={6} xs={12} style={{ margin: "auto" }}>
+          <Grid item md={6} xs={12} style={{ margin: 'auto' }}>
             <Paper style={{ padding: 30, marginTop: 20 }} elevation={1}>
               <Typography paragraph={true} variant="body1">
                 <a
@@ -151,11 +154,11 @@ class HomePage extends React.Component {
             </Paper>
           </Grid>
         </Fragment>
-      );
+      )
     } else {
-      return null;
+      return null
     }
   }
 }
 
-export default HomePage;
+export default HomePage
