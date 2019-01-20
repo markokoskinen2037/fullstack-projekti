806c8797f4f9b8ae63c9f100095fdd9df7d31685
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-20 21:13:25 +0200

    autoformat

--- frontend/aikatauluttaja/src/services/courses.js
+++ frontend/aikatauluttaja/src/services/courses.js
@@ -1,55 +1,58 @@
-import axios from "axios"
-const baseUrl = "/api/courses"
+import axios from "axios";
+const baseUrl = "/api/courses";
 
-let token = null
+let token = null;
 
-const setToken = (newToken) => {
-    token = `bearer ${newToken}`
-}
+const setToken = newToken => {
+  token = `bearer ${newToken}`;
+};
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-const get = (id) => {
-    const request = axios.get(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
-}
+const get = id => {
+  const request = axios.get(`${baseUrl}/${id}`);
+  return request.then(response => response.data);
+};
 
-const create = async (newObject) => {
-    const config = {
-        headers: { 'Authorization': token }
-    }
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  };
 
-    const response = await axios.post(baseUrl, newObject, config)
-    return response
-}
+  const response = await axios.post(baseUrl, newObject, config);
+  return response;
+};
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response => response.data);
+};
 
 const removeById = (id, userId) => {
+  const config = {
+    headers: { Authorization: userId }
+  };
 
-    const config = {
-        headers: { 'Authorization': userId }
-    }
-
-    axios.delete(`${baseUrl}/${id}`, config)
-
-}
+  axios.delete(`${baseUrl}/${id}`, config);
+};
 
 const deleteAllCourses = async () => {
-    const allCourses = await getAll()
+  const allCourses = await getAll();
 
+  allCourses.forEach(course => {
+    removeById(course._id);
+  });
+};
 
-    allCourses.forEach(course => {
-        removeById(course._id)
-    });
-
-}
-
-
-export default { getAll, create, update, get, removeById, setToken, deleteAllCourses }
+export default {
+  getAll,
+  create,
+  update,
+  get,
+  removeById,
+  setToken,
+  deleteAllCourses
+};
