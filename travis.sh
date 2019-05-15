528b207b769bc6e9242c3866c90a761104b3b970
Marko Koskinen <markokoskinen2037@gmail.com>
2019-05-15 17:59:02 +0300

    travis

--- travis.sh
+++ travis.sh
@@ -2,9 +2,11 @@
 echo RUNNING TRAVIS.SH SCRIPT
 mkdir pushmetoheroku
 cd frontend/aikatauluttaja/
+npm install
 npm run build
 cp -r build ../../backend/
 cd ../../backend
+npm install
 cp -r . ../../pushmetoheroku/
 cd ../../pushmetoheroku
 echo Ollaan kansiossa:
