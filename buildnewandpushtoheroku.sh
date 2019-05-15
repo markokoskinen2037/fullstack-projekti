848cda9fbb070fd22d20f43d1d7566c52056979e
Marko Koskinen <markokoskinen2037@gmail.com>
2019-05-15 23:28:33 +0300

    modify the script

--- buildnewandpushtoheroku.sh
+++ buildnewandpushtoheroku.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+mkdir pushmetoheroku
+cd pushmetoheroku
+git init
+heroku git:remote -a opintojenaikatauluttaja
+cd ..
 cd frontend/aikatauluttaja/
 npm run build
 cp -r build ../../backend/
 cd ../../backend
-cp -r . ../../pushmetoheroku/
-cd ../../pushmetoheroku
+cp -r . ../pushmetoheroku/
+cd ../pushmetoheroku
+
 git add .
 git commit -m "new automatic commit"
 git push heroku master
